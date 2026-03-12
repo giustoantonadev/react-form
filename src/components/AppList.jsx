43c817146a0f620c52fbedae5b31a0850620651a
giustoantonadev <giustoantonadev@gmail.com>
2026-03-12 14:35:13 +0100

    array change form with submit button

--- src/components/AppList.jsx
+++ src/components/AppList.jsx
@@ -1,48 +1,36 @@
 import { useState } from "react"
 
 const articlesList = [
-    {
-        id: 1,
-        title: 'Introduzione a React',
-        text: 'testo articolo React'
-    },
-    {
-        id: 2,
-        title: 'Come funziona lo stato in React',
-        text: 'testo articolo stato React'
-
-    },
-    {
-        id: 3,
-        title: 'Gestire eventi nei componenti',
-        text: 'testo articolo eventi componenti React'
-    },
-    {
-        id: 4,
-        title: 'Differenza tra props e state',
-        text: 'testo articolo differenza props e state React'
-    },
-    {
-        id: 5,
-        title: 'Creare il primo blog con React',
-        text: 'testo articolo primo blog creato usando React'
-    }
+    'Introduzione a React',
+    'Come funziona lo stato in React',
+    'Gestire eventi nei componenti',
+    'Differenza tra props e state',
+    'Creare il primo blog con React'
 ];
 
 export default function AppList() {
 
     const [articles, setArticles] = useState(articlesList)
+    const [newArticle, setNewArticle] = useState('')
+
+    function submitArticle(e) {
+        e.preventDefault()
+        setArticles([newArticle, ...articles])
+    }
 
     return (
 
         <div className="container">
-
-            <div className="card">
+            <div className="card text-center p-5">
+                <h1 className="text-center">Articles</h1>
+                <form onSubmit={submitArticle}>
+                    <input type='text' value={newArticle} onChange={e => setNewArticle(e.target.value)} />
+                    <button type='submit' className="btn btn-primary m-1">Add New Article</button>
+                </form>
                 <ul className="list-group" >
-                    <h1 className="text-center">Articles</h1>
-                    {articles.map(article => (
-                        <li key={article.id} className="list-group-item d-flex justify-content-between" >
-                            {article.title}
+                    {articles.map((article,index) => (
+                        <li key={index} className="list-group-item d-flex justify-content-between" >
+                            {article}
                             <button className="badge text-bg-warning rounded-pill" >DELETE</button>
                         </li>
                     ))}
